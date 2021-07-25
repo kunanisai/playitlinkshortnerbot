f8e894921dcf26a080a0bbc44df6291ca6637bfb
K.S PROJECTS <86557294+ksprojects1@users.noreply.github.com>
2021-07-25 19:18:39 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# GPLinksBot 🔥
+Make short link by using [GPlink](https://gplinks.in/) API key
+### Installation
+
+#### The Easy Way
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://www.heroku.com/deploy?template=https://github.com/CyberBoyAyush/GPLinksBot)
+
+## Try GPLinks PHP Bot (Some Are getting Error In This Repo So Use PHP Repo. If You Are Getting Error)
+https://github.com/CyberBoyAyush/Gp-Links-Bot
+
+##### Required Variables
+
+* `BOT_TOKEN`: Create a bot using [@BotFather](https://telegram.dog/BotFather), and get the Telegram API token.
+
+* `API_ID`: Get this value from [telgram.org](https://my.telegram.org/apps) Or By Telegram Bot [TgApiextractorBot](https://telegram.dog/TgApiextractorBot)
+* `API_HASH`: Get this value from [telgram.org](https://my.telegram.org/apps)
+* `API_KEY`: Create [GPlink](https://gplinks.in/) account then get this value from [GPlink API](https://gplinks.in/member/tools/api)
+
+##### Credit
+* [Mahesh Bhai](https://github.com/Mahesh0253) For His Repo
+* [Me Cyber Boy Ayush](https://github.com/CyberBoyAyush) Editing This Repo
---
+++ bot.py
@@ -0,0 +1,45 @@
+from os import environ
+import aiohttp
+from pyrogram import Client, filters
+
+API_ID = environ.get('API_ID')
+API_HASH = environ.get('API_HASH')
+BOT_TOKEN = environ.get('BOT_TOKEN')
+API_KEY = environ.get('API_KEY', '5fd20df0c4db85798dd4f5ff3d03e3606a94f98b')
+
+bot = Client('gplink bot',
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+             workers=50,
+             sleep_threshold=10)
+
+
+@bot.on_message(filters.command('start') & filters.private)
+async def start(bot, message):
+    await message.reply(
+        f"**𝗛𝗘𝗟𝗟𝗢🎈{message.chat.first_name}!**\n\n"
+        "𝗜'𝗺 𝗚𝗣𝗹𝗶𝗻𝗸 𝗯𝗼𝘁. 𝗝𝘂𝘀𝘁 𝘀𝗲𝗻𝗱 𝗺𝗲 𝗹𝗶𝗻𝗸 𝗮𝗻𝗱 𝗴𝗲𝘁 𝗦𝗵𝗼𝗿𝘁𝗲𝗻𝗲𝗱 𝗨𝗥𝗟. \n\n 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗜𝘀 𝗠𝗮𝗱𝗲 𝗕𝘆 @CyberBoyAyush💖")
+
+
+@bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
+async def link_handler(bot, message):
+    link = message.matches[0].group(0)
+    try:
+        short_link = await get_shortlink(link)
+        await message.reply(f'Here is your👉 [Short Link🎈]({short_link})', quote=True)
+    except Exception as e:
+        await message.reply(f'Error: {e}', quote=True)
+
+
+async def get_shortlink(link):
+    url = 'https://gplinks.in/api'
+    params = {'api': API_KEY, 'url': link}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True) as response:
+            data = await response.json()
+            return data["shortenedUrl"]
+
+
+bot.run()
